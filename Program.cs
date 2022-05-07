662f539494ed0acda6014ab5f2c3636e18ae61e7
miguellorenzo123 <lorenzo142114@hotmail.com>
2022-05-07 18:10:11 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba_moansoxd
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+
+            int n1, n2;
+            Console.WriteLine("ingrese un numero: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro numero: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("la suma de los 2 numeros ingresado es: " + (n1 + n2));
+
+
+        }
+}
+}
